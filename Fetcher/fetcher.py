5ba2d1fb6f8e20afca00cb79639c2355f29e95f5
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-03 20:01:28 -0300

    Started migration to Twython

--- Fetcher/fetcher.py
+++ Fetcher/fetcher.py
@@ -3,12 +3,17 @@ Created on Nov 20, 2018
 
 @author: Gabriel Torrandella
 '''
-
-import twitter
 import Tweet
+from twython import Twython
+
+APP_KEY = "7zCU1BgDeQ3G65MfwvpNUZm3a"
+APP_SECRET = "0Kii63THccgKCKMvE396GgaieUld5HtADLeU98wJmlpiWzfP47"
+ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAAHqS8wAAAAAAC%2FBQkp9ZMJ3igj1orr2ou%2BBzQKM%3DgodQO6bVl2TLlhjPZCm1hT2MbwTwXIdJj5Upc9oCPWE3mjBYIK"
+
 
 class Fetcher():
-    api = twitter.Api()
+    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
     
     def fetchByHashtag(self, hashtag, lastId):
         rawTweet = []
