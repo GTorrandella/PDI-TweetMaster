9ec73d1cb122f54cdabf6d8cbb155e26c2e7fa2c
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-04 19:51:27 -0300

    Created the flask server for Fetcher

---
+++ Fetcher/fetcher_flask.py
@@ -0,0 +1,30 @@
+'''
+Created on Dec 04, 2018
+
+@author: Gabriel Torrandella
+'''
+from flask import Flask, json, jsonify
+from fetcher import Fetcher
+from flask.globals import request
+from flask.wrappers import Response
+
+app = Flask(__name__)
+
+@app.route('/fetcher', methods = ['GET'])
+def api_fetcher():
+    
+    if request.headers['Content-Type'] == 'application/json':
+        requestJson = json.dumps(request.json)
+        campaign = requestJson['Campaing']
+        lastId = requestJson['Last-ID']
+        tweets =  Fetcher().fetchTweets(campaign, lastId)
+        resp = jsonify(tweets)
+        resp.status_code = 200
+        return resp
+        
+    else:
+        return Response(status=400)
+
+
+if __name__ == "__main__":
+    app.run()
