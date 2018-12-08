3c1e6ac2a81702aecf61b8223a3a4c761dd0a003
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-07 23:05:58 -0300

    Added the Twython mock

--- Fetcher/tests/test_fetcher_flask.py
+++ Fetcher/tests/test_fetcher_flask.py
@@ -7,17 +7,27 @@ import unittest
 from tests.test_fetcher_base import test_fetcher_base
 
 import fetcher_flask
+from fetcher import Fetcher
+from unittest.mock import MagicMock
 
 class test_fetcher_flask(test_fetcher_base):
 
+    def return_values(self, search, q, result_type, since_id):
+        if q == "#mars":
+            return self.responseHastag
+        
+        if q == "@mars":
+            return self.responseMention
 
+        
     def setUp(self):
         test_fetcher_base.setUp(self)
         
         self.test_app = fetcher_flask.app.test_client()
-        self.test_app
         self.ctx = fetcher_flask.app.app_context()
         self.ctx.push()
+        
+        Fetcher().twitter.cursor = MagicMock(side_effect = self.return_values)
 
     def tearDown(self):
         self.ctx.pop()
