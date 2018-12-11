56da5787b1ab36959b57f52bb1aef81311f8e1ae
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-11 17:18:15 -0300

    Updated call to manager class

--- Manager/manager_flask.py
+++ Manager/manager_flask.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask.globals import request
 from flask.wrappers import Response
 from werkzeug.wrappers import ETagRequestMixin, ETagResponseMixin, BaseRequest, BaseResponse
-import manager
+from manager import Manager
 
 app = Flask(__name__)
 
@@ -25,7 +25,7 @@ def api_manager():
     
     if request.method == 'POST':
         if checkForm(request.form):
-            idCampaing = manager.insertCampaign(request.form)
+            idCampaing = Manager.insertCampaign(request.form)
             res = Response(status_code = 201)
             res.set_etag(idCampaing, weak = False)    
             return res
@@ -34,9 +34,9 @@ def api_manager():
         
     elif request.method == 'DELETE':
         if 'idC' in request.json:
-            manager.deleteCampaignporid(request.json['idC'])
+            Manager.deleteCampaignporid(request.json['idC'])
         elif 'email' in request.json:
-            manager.deleteCampaignporuser(request.json['email'])
+            Manager.deleteCampaignporuser(request.json['email'])
         return Response(status_code = 200)
         
     else: 
@@ -46,13 +46,13 @@ def api_manager():
 def api_manager_id(idC):
     
     if request.method == 'GET':
-        jsonCampaing = manager.returnCampaign(idC).to_json()
+        jsonCampaing = Manager.returnCampaign(idC).to_json()
         res = Response(jsonCampaing, status = 200, mimetype = 'application/json')
         return res
         
     elif request.method == 'PATCH':
         if ('columnaAModif' in request.json) and ('campoColumna' in request.json):
-            manager.modifyCampaign(idC, request.json['columnaAModif'], request.json['campoColumna'])
+            Manager.modifyCampaign(idC, request.json['columnaAModif'], request.json['campoColumna'])
             return Response(status_code = 202)
         else:
             return Response(status_code = 404)
