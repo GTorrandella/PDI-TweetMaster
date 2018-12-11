c84f0d68f0f1df97003f00b23722adc8e0df58ca
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-11 15:17:00 -0300

    Finished POST method

--- Manager/manager_flask.py
+++ Manager/manager_flask.py
@@ -4,25 +4,31 @@ Created on Dec 10, 2018
 @author: Gabriel Torrandella
 '''
 from flask import Flask, json
+from wtforms import Form
 from flask.globals import request
 from flask.wrappers import Response
 from Campaign import Campaign
-from Manager.manager import *
+import manager
 
 app = Flask(__name__)
 
 def checkForm(form):
+    return True
 
 @app.route('/Campaing', methods = ['POST', 'DELETE'])
 def api_manager():
     
     if request.method == 'POST':
-        if checkForm(request.form)
-        
-        return request(status_code = 201)
+        if checkForm(request.form):
+            idCampaing = manager.insertCampaign(request.form)
+            res = Response(status_code = 201)
+            res.set_etag(idCampaing)    
+            return res
+        else:
+            return Response(status_code = 412)
         
     if request.method == 'DELETE':
-        return request(status_code = 200)
+        return Response(status_code = 200)
         
     else: 
         return Response(status_code = 400)
