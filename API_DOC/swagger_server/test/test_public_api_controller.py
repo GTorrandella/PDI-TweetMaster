bf2532043e2a2727ff32102885b08ddd4c2b6172
Federico Calonge <calongefederico@gmail.com>
2018-12-11 14:16:40 -0300

    Cambios en el manager, Connector e Interfaz Web

---
+++ API_DOC/swagger_server/test/test_public_api_controller.py
@@ -0,0 +1,98 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.campaign import Campaign  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestPublicAPIController(BaseTestCase):
+    """PublicAPIController integration test stubs"""
+
+    def test_add_campaign(self):
+        """Test case for add_campaign
+
+        Añade una Campaña.
+        """
+        Campaign = Campaign()
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Campaign',
+            method='POST',
+            data=json.dumps(Campaign),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_del_campaign(self):
+        """Test case for del_campaign
+
+        Eliminar una campaña.
+        """
+        headers = [('_email', '_email_example'),
+                   ('_idC', 56)]
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Campaign',
+            method='DELETE',
+            headers=headers,
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_campaign(self):
+        """Test case for get_campaign
+
+        Obtener una Campaña.
+        """
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Campaign/{_idC}'.format(_idC=56),
+            method='GET',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_reporter_json(self):
+        """Test case for get_reporter_json
+
+        Obtener un resumen del reporte en JSON.
+        """
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Reporter/ReporterJSON/{_idC}'.format(_idC=56),
+            method='GET',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_reporter_raw(self):
+        """Test case for get_reporter_raw
+
+        Obtener un reporte crudo.
+        """
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Reporter/ReporterRaw/{_idC}'.format(_idC=56),
+            method='GET',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_mod_campaign_byid_c(self):
+        """Test case for mod_campaign_byid_c
+
+        Modificar una Campaña.
+        """
+        headers = [('columnaAModif', 'columnaAModif_example'),
+                   ('campoColumna', 'campoColumna_example')]
+        response = self.client.open(
+            '/FedericoCalonge/TweetMaster/1.0.0/Campaign/{_idC}'.format(_idC=56),
+            method='PATCH',
+            headers=headers,
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
--- DataBaseConnector/configTables.py
+++ DataBaseConnector/configTables.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import relationship
 
 #Es nuestra abstraccion de la base de datos:
 engine = create_engine("mysql+pymysql://root:4236@localhost:3306/BDTweetMaster", echo=True)
@@ -17,41 +18,21 @@ session = Session()
 class Campaign(BD):
 	__tablename__ = 'campaign'
 
-	idC = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True)
 	startDate = Column(String(10))
 	finDate = Column(String(10))
 	email = Column(String(40))
-	#hashtags = relationship('HashTag')
-	#mentions = relationship('Mention')
+	hashtags = Column(String(40))
+	mentions = Column(String(40))
     # hook = Column(String(10))
 
 	def __repr__(self):
-   	    return "<Campaign(idC='%s', startDate='%s', finDate='%s', email='%s')>" % (self.idC, self.startDate, self.finDate, self.email)
-
-class HashTag(BD):
-	__tablename__ = 'hashTag'
-	
-	idH = Column(Integer, primary_key=True)
-	hashtag = Column(String(10))
-	idCampaign = Column(Integer, ForeignKey('campaign.idC'))
-	
-	def __repr__(self):
-		return "<HashTag(hashtag='%s', idCampaign='%s')>" % (self.hashtag, self.idCampaign)
-
-class Mention(BD):
-	__tablename__ = 'mention'
-	
-	idU = Column(Integer, primary_key=True)
-	user_mentions = Column(String(10))
-	idCampaign = Column(Integer, ForeignKey('campaign.idC'))
-	
-	def __repr__(self):
-		return "<Mention(user_mention='%s', idCampaign='%s')>" % (self.usuario, self.idCampaign)
+   	    return "<Campaign(idC='%s', startDate='%s', finDate='%s', email='%s', hashtags='%s', mentions='%s')>" % (self.id, self.startDate, self.finDate, self.email, self.hashtags, self.mentions)
 
 class Tweets(BD):
 	__tablename__ = 'tweets'
 	
-	idT = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True)
 	fechaPublicacion = Column(String(50))
 	autor=Column(String(10))
 	mensaje=Column(String(10))
