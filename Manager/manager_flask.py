833e1036e311420305978560864bbd0a86c73e27
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-11 15:52:02 -0300

    Finished Campaing DELETE, started Campaign/{_idC} GET & PATCH

--- Manager/manager_flask.py
+++ Manager/manager_flask.py
@@ -7,10 +7,17 @@ from flask import Flask, json
 from wtforms import Form
 from flask.globals import request
 from flask.wrappers import Response
+from werkzeug.wrappers import ETagRequestMixin, ETagResponseMixin, BaseRequest, BaseResponse
 from Campaign import Campaign
 import manager
 
 app = Flask(__name__)
+
+class Response(BaseResponse, ETagResponseMixin):
+    pass
+
+class Resquest(BaseRequest, ETagRequestMixin):
+    pass
 
 def checkForm(form):
     return True
@@ -22,18 +29,31 @@ def api_manager():
         if checkForm(request.form):
             idCampaing = manager.insertCampaign(request.form)
             res = Response(status_code = 201)
-            res.set_etag(idCampaing)    
+            res.set_etag(idCampaing, weak = False)    
             return res
         else:
             return Response(status_code = 412)
         
-    if request.method == 'DELETE':
-        try:
-            manager.deleteCampaignporid(re)
+    elif request.method == 'DELETE':
+        manager.deleteCampaignporid(request.get_etag())
         return Response(status_code = 200)
         
     else: 
         return Response(status_code = 400)
+
+@app.route('/Campaing/<int:idC>', methods = ['GET', 'PATCH'])
+def api_manager_id():
+    
+    if request.method == 'GET':
+        pass
+    
+    elif request.method == 'PATCH':
+        pass
+    
+    else:
+        return Response(status_code = 404)
+    
+    pass
 
 if __name__ == "__main__":
     app.run()
