47adb4220ac79d9793e9a50b89bc26fcaf125c1b
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-11 18:43:23 -0300

    Scheduler class ready

---
+++ Scheduler/scheduler.py
@@ -0,0 +1,27 @@
+'''
+Created on Dec 10, 2018
+
+@author: Gabriel Torrandella
+'''
+from DataBseConnector.Connector import returnCampaingsInProgress
+from manager import Manager
+from _datetime import datetime
+
+class Scheduler():
+    
+    def getTime(self):
+        return datetime.now().strftime("%d %m %Y %X")
+    
+    def getCampaings(self, date):
+        return returnCampaingsInProgress(date)
+    
+    def wakeManager(self, campaigns):
+        Manager.fetchCampaings(campaigns)
+        
+    def keepSchedule(self):
+        date = self.getTime()
+        camps = self.getCampaings(date)
+        if not len(camps) == 0:
+            self.wakeManager(camps)
+
+Scheduler.keepSchedule()
