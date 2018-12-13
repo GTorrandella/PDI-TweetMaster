f26a54430ba529189fe04e52d505915167d3358a
Gabriel Torrandella <torrandellag@gmail.com>
2018-12-12 22:07:25 -0300

    Fixed imports

--- Reporter/Reporter.py
+++ Reporter/Reporter.py
@@ -1,20 +1,20 @@
 import sys
 sys.path.append("..")
 import json
-from Campaign.Campaign import *
-from Manager.manager import Manager
-from dataBaseConnector import Connector
+from Campaign import Campaign
+from manager import Manager
+import Connector
 
 class Reporter():
 
-	def getcampaign(idC):
+	def getcampaign(self,idC):
 		c_json = Connector().retornarCampaignBD(idC)	#Traer campaña de la BD
 		c_dict = json.loads(c_json) 			#json a diccionario
 		campaign = Campaign(c_dict("id"), c_dict("email"), c_dict("hashtags"), c_dict("mentions"), c_dict("startDate"), c_dict("finDate"))
 		return campaign
 	
-	def reportRawData(idC):
-		campaign = getcampaign(idC)
+	def reportRawData(self,idC):
+		campaign = self.getcampaign(idC)
 		#tweets = Connector().returnTweetsByIDC()  #Busca tweets de determinada campaña (falta hacer en Connector)
 		
 		for t in tweets:
@@ -22,11 +22,17 @@ class Reporter():
 
 		return (rawData)
 
-	def reportSummary(campaign): 
-		campaign = getcampaign(idC)
+	def countTweets(self, tweets):
+		cant_tweets = 0
+        for cant_tweets in range tweets:
+        	cant_tweets = cant_tweets +1
+        	return cant_tweets
+        
+	def reportSummary(self, idC): 
+		campaign = self.getcampaign(idC)
 		#tweets = Connector().returnTweetsByIDC()  #Busca tweets de determinada campaña (falta hacer en Connector
-
-        cant_tweets = countTweets(tweets)			#contar cantidad de tweets	
+		
+		cant_tweets = self.countTweets(tweets)			#contar cantidad de tweets	
         dictionary = ["cant_tweets"] = cant_tweets	#lo agrego al diccionario
         summary = json.dumps(dictionary)
         #Cantidad diferente de usuarios
@@ -35,9 +41,4 @@ class Reporter():
 
 		campaignJson = campaign.to_json()
 
-	def countTweets(tweets):
-		cant_tweets = 0
-        for cant_tweets in range tweets:
-        	cant_tweets = cant_tweets +1
-        	return cant_tweets
 
