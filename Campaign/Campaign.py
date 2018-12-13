d0d32cb40feadeedaffe5adbd5b0c6621928323c
JuanDavid24 <31551453+JuanDavid24@users.noreply.github.com>
2018-12-13 13:21:51 -0300

    Agregado método to_dict
    Volvieron los getters y setters que no estaban en esta versión pero en master sí. No se por qué. Se los sacan si quieren.

--- Campaign/Campaign.py
+++ Campaign/Campaign.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from _datetime import datetime
 import json
 
 class Campaign(object):
@@ -12,7 +13,48 @@ class Campaign(object):
         self.startDate = datetime.strptime(startDate, "%d %m %Y %X") #dd mm yyyy hh:mm:ss
         self.finDate = datetime.strptime(finDate, "%d %m %Y %X") #dd mm yyyy hh:mm:ss
 
+    def get_idC(self):
+        return self.__idC
+
+    def get_emailDueño(self):
+        return self.__emailDueño
+
+    def get_hashtags(self):
+        return self.__hashtags
+
+    def get_mentions(self):
+        return self.__mentions
+
+    def get_start_date(self):
+        return self.__startDate
+
+    def get_fin_date(self):
+        return self.__finDate
+
+    def set_idC(self, value):
+        self.__idC = value
+
+    def set_emailDueño(self, value):
+        self.__emailDueño = value
+
+    def set_hashtags(self, value):
+        self.__hashtags = value
+
+    def set_mentions(self, value):
+        self.__mentions = value
+
+    def set_start_date(self, value):
+        self.__startDate = value
+
+    def set_fin_date(self, value):
+        self.__finDate = value
+
     def to_json(self):
+        dictionary = self.to_dict()
+        camp_json = json.dumps(dictionary) 
+        return camp_json
+
+    def to_dict(self):
         dictionary = {
             "id" : self.__idC,
             "email" : self.__emailDueño,
@@ -21,10 +63,16 @@ class Campaign(object):
             "startDate" : str(self.__startDate),
             "finDate" : str(self.__finDate),
         }
-        camp_json = json.dumps(dictionary) 
-        return camp_json
+        return dictionary
 
     def __repr__(self):
         return "<idC:%s emailDueño:%s hashtags:%s mentions:%s startDate:%s finDate:%s> " % (self.idC, self.emailDueño, self.hashtags, self.mentions, self.startDate, self.finDate)
+    
+    idC = property(get_idC, set_idC, "idC's docstring")
+    emailDueño = property(get_emailDueño, set_emailDueño, "emailDueño's docstring")
+    hashtags = property(get_hashtags, set_hashtags, "hashtags's docstring")
+    mentions = property(get_mentions, set_mentions, "mentions's docstring")
+    startDate = property(get_start_date, set_start_date, "startDate's docstring")
+    finDate = property(get_fin_date, set_fin_date, "finDate's docstring")
         
     
