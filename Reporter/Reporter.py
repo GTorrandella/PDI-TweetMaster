60a5ab8aa4d3af07d0ca3a826c5282d5a7830b5b
Federico Calonge <38995964+FedericoCalonge@users.noreply.github.com>
2018-12-20 15:49:37 -0300

    campaña inexistente o sin tweets devuelve 404
    En reportRawData y reportSummary se devuelven objetos vacios si no hay tweets o campaña

--- Reporter/Reporter.py
+++ Reporter/Reporter.py
@@ -1,5 +1,5 @@
 import json
-import DataBaseConnector.Connector as Connector
+from DataBaseConnector import Connector as Connector
 from collections import Counter
 
 class Reporter():
@@ -7,19 +7,25 @@ class Reporter():
 	def reportRawData(self, idC): #OK
 		campaign = Connector.retornarCampaignBD(idC) #Objeto campaign, NO desempaquetamos el JSON, esto lo hace directamente flask. 
 		tweets = Connector.returnTweetsByIDC(idC) #Busca tweets de determinada campaña
-		rawData = {"campaign" : campaign.to_dict(), "tweets" : tweets}
+		if (tweets==[] or campaign==None):
+			rawData=None
+		else:	
+			rawData = {"campaign" : campaign.to_dict(), "tweets" : tweets}
 		return (rawData)
 
 	def reportSummary(self, idC): #OK
 		campaign = Connector.retornarCampaignBD(idC) #Objeto campaign, NO desempaquetamos el JSON, esto lo hace directamente flask. 
 		tweets = Connector.returnTweetsByIDC(idC) #Lista de diccionarios tweet
 
-		summary = {
-			"campaign" : campaign.to_dict(), #como diccionario para que se pueda acceder a los campos mas facil
-			"cant_tweets" : len(tweets),
-			"moreTwUser" : self.getUserWithMoreTw(tweets), #Autor con mas tweets
-			"userQuantity": self.getUserQuantity(tweets), #Cantidad diferente de usuarios
-		}
+		if (tweets==[] or campaign==None):
+			summary=None
+		else:	
+			summary = {
+				"campaign" : campaign.to_dict(), #como diccionario para que se pueda acceder a los campos mas facil
+				"cant_tweets" : len(tweets),
+				"moreTwUser" : self.getUserWithMoreTw(tweets), #Autor con mas tweets
+				"userQuantity": self.getUserQuantity(tweets), #Cantidad diferente de usuarios
+			}
 		return (summary)
 
 	def getUserWithMoreTw(self,tweets): #OK (falta el caso en que sean varios users en el 1er puesto)
--- Reporter/reporter_flask.py
+++ Reporter/reporter_flask.py
@@ -3,30 +3,32 @@ Created on Dec 12, 2018
 
 @author: Gabriel Torrandella
 '''
-from flask import Flask, json
+from flask import Flask, jsonify
 from flask.globals import request
 from flask.wrappers import Response
 
-from Reporter import Reporter
+from Reporter.Reporter import Reporter
 
 app = Flask(__name__)
 
-@app.route('/Reporter/ReporterJSON/<int:idC> ', methods = ['GET'])
+@app.route('/Reporter/ReporterJSON/<int:idC>', methods = ['GET'])
 def api_report_json(idC):
-
     if True: # Aca se fija si existe el recurso
-        json = Reporter().reportSummary(idC)
-        return Response(json, status = 200, mimetype='aplication/json')
-            
+        summary = Reporter().reportSummary(idC)
+        if not summary:
+            return Response(status=404)
+        return jsonify(summary)    
     else:
         return Response(status = 404)
     
-@app.route('/Reporter/ReporterRAW/<int:idC> ', methods = ['GET'])
+@app.route('/Reporter/ReporterRAW/<int:idC>', methods = ['GET'])
 def api_report_raw(idC):
     
     if True:
         data = Reporter().reportRawData(idC)
-        return Response(data, status = 200)
+        if not data:
+            return Response(status = 404)
+        return jsonify(data)
     else:
         return Response(status = 404)
 
