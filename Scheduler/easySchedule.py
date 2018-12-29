26cd36ad1a55f27d3dd0c70c839716ac051cf6fd
Federico Calonge <calongefederico@gmail.com>
2018-12-29 20:24:46 -0300

    Cambio en easyscheduler

--- Scheduler/easySchedule.py
+++ Scheduler/easySchedule.py
@@ -1,15 +1,11 @@
-import schedule
 import time
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from Manager.manager import Manager
 
-def job():
-    Manager().fetchCampaings()
-
-schedule.every(5).minutes.do(job)
-
 while True:
-    schedule.run_all()
-    #time.sleep(1)
+	print("Llamada a Manager")
+	Manager().fetchCampaings()
+	#schedule.run_all(delay_seconds=5)
+	time.sleep(120) #Cada 2 minutos llama al manager.
